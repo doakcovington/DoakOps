6e66659dcadd18904e9048b466971c47e00f3e25
doak <doakcovington@protonmail.com>
2023-02-15 14:34:36 -0800

    update

--- main.tf
+++ main.tf
@@ -38,35 +38,9 @@ resource "azurerm_static_site" "doakops-site" {
   }
 }
 
-resource "azurerm_key_vault" "example" {
-  name                        = "doakops-site-keyvault"
-  location                    = azurerm_resource_group.doakops-rg.location
-  resource_group_name         = azurerm_resource_group.doakops-rg.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Get",
-      "Create",
-      "List",
-      "Delete",
-    ]
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Delete"
-    ]
-
-    storage_permissions = [
-      "Get",
-    ]
-  }
+resource "azurerm_dns_zone" "doakops-dns" {
+  name                = "doakops.com"
+  resource_group_name = azurerm_resource_group.doakops-rg.name
 }
 
 
