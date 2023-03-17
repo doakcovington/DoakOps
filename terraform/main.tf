03113f697b1998467bca68e982c07a3086412eb8
Doak <doakcovington@protonmail.com>
2023-03-16 19:55:21 -0700

    added vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,16 +20,28 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
+variable "resource_group_name" {
+  default = "devops-dev"
+}
+
+variable "static_site_name" {
+  default = "doakops-dev"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
 resource "azurerm_resource_group" "doakops-rg" {
-  name     = "doakops-prod"
+  name     = var.resource_group_name
   location = "westus2"
   tags = {
-    environment = "prd"
+    environment = var.environment
   }
 }
 
 resource "azurerm_static_site" "example" {
-  name                = "doakops-prod"
+  name                = var.static_site_name
   resource_group_name = azurerm_resource_group.doakops-rg.name
   location            = azurerm_resource_group.doakops-rg.location
 }
