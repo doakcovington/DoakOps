ee07ea661c05139510523652db311f00322cbdc7
doak <doakcovington@protonmail.com>
2023-03-26 19:25:53 -0700

    cleanup

--- terraform/dev/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-variable "dns-zone" {
-  type = string
-  default = "dev-doakops.com"
-}
--- terraform/site.tf
+++ terraform/site.tf
@@ -1,17 +1,4 @@
 data "azurerm_client_config" "current" {}
-
-variable "resource_group_name" {
-  default = "doakops-dev"
-}
-
-variable "static_site_name" {
-  default = "doakops-dev"
-}
-
-variable "environment" {
-  default = "dev"
-}
-
 resource "azurerm_resource_group" "doakops-rg" {
   name     = var.resource_group_name
   location = "westus2"
@@ -25,6 +12,5 @@ resource "azurerm_static_site" "example" {
   resource_group_name = azurerm_resource_group.doakops-rg.name
   location            = azurerm_resource_group.doakops-rg.location
 }
-
 
 
