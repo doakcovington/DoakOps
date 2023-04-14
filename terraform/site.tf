242cd048e1ead056b8ac83646a69b7d9d4752b27
doak <doakcovington@protonmail.com>
2023-04-13 19:26:12 -0700

    add vm

--- terraform/site.tf
+++ terraform/site.tf
@@ -1,16 +1,9 @@
-data "azurerm_client_config" "current" {}
-resource "azurerm_resource_group" "doakops-rg" {
-  name     = var.resource_group_name
-  location = "westus2"
-  tags = {
-    environment = var.environment
-  }
-}
+# data "azurerm_client_config" "current" {}
 
-resource "azurerm_static_site" "example" {
-  name                = var.static_site_name
-  resource_group_name = azurerm_resource_group.doakops-rg.name
-  location            = azurerm_resource_group.doakops-rg.location
-}
+# resource "azurerm_static_site" "example" {
+#   name                = "doakops"
+#   resource_group_name = azurerm_resource_group.doakops.name
+#   location            = azurerm_resource_group.doakops.location
+# }
 
 
